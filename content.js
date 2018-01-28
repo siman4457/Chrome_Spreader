b92a516b8eba9c027c0f9a3ee8beb8f7d4a1d5e6
consmith <camcon97@gmail.com>
2018-01-28 02:09:23 -0600

    Creating Dictionary of words present

--- content.js
+++ content.js
@@ -28,7 +28,6 @@ article.appendChild(stuff);
 
 
 
-
 var article_text = document.getElementsByTagName('p');
 
 
@@ -37,14 +36,16 @@ var bod = document.getElementsByTagName('body');
 var entire = document.getElementsByTagName('html');
 */
 
-
+stuff.innerHTML += "<pre>";
 for(i = 0; i < article_text.length; i++) {
 	//Not needed and unnecessary:
 	let p_list = article_text[i];
 	console.log(p_list);
 	//document.getElementsByClassName("text")[0].appendChild(x[i]);
 	//stuff.appendChild(p_list);
+	
 	stuff.innerHTML += p_list.innerHTML;
+	stuff.innerHTML += "\n";
 	/*
 	for(var i = 0; i <= x[i].children.length; i++){
 		x[i].style.display = "inherit";
@@ -53,9 +54,28 @@ for(i = 0; i < article_text.length; i++) {
 	
 }
 
-main.innerHTML = article_text[2].innerHTML;
+stuff.innerHTML += "</pre>";
+
+main.innerHTML = article_text[0].innerHTML;
 
 console.log(article_text[2].innerHTML);
+
+//var input_text = “This is a test. This should test the speed reader and display it”;
+
+var Array_Words = [];
+
+for(i = 0; i < article_text.length; i++){
+	var par_string = article_text[i].innerHTML;
+	for (j = 0; j < par_string.length; j++){
+ 		Array_Words.push(par_string.split(""));
+	}
+}
+
+
+for (var j = 0; j < Array_Words.length; j++){
+   console.log(Array_Words[j]);
+   }
+//console.log(Array_Words);
 
 
 
